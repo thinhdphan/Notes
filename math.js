0bc3e83ff9401c1b216b6df0efb75f41334c0381
tphan1996 <107013661+thinhdphan@users.noreply.github.com>
2023-05-18 13:20:25 -0500

    Math Notes

---
+++ math.js
@@ -0,0 +1,34 @@
+/* 
+  Arithmatic expressioon is a combinaton of..
+  Operands ( values, variables, etc.)
+  Operators ( + - * / %)
+  that can be evaluated to  a value
+  Example: y = x + 5;
+
+*/
+
+/* 
+  + Add
+  - Substract
+  * Multiply
+  / Divide
+  % Remainder ( can find even or odd )
+*/
+/* 
+  Operator precedence
+  1. Parenthesis ()
+  2. Exponents
+  3. Multiplication & Division
+  4. Addition & Substraction
+*/
+
+let students = 20;
+
+// All the same thing
+students = students + 1;
+students += 1;
+students++;
+
+console.log(students);
+
+
