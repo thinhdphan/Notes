0c6994c564f62e012e55dacf9fc843c74576b572
tphan1996 <107013661+thinhdphan@users.noreply.github.com>
2023-05-18 13:10:49 -0500

    Variables Notes

---
+++ variables.js
@@ -0,0 +1,29 @@
+// A container for storing data 
+/* A variable behaves as if it was the value that it contains */
+// Can store any value in a variable 
+// BE SURE TO USE CamelCase when naming variables
+// Example: firstName , lastName, birthDate, etc.
+
+// Scope 
+/* Variables within the scope means you can only use the variable that lies within the bracket */
+
+// Global scope 
+/* Variables that are outside of the scope and can be use from anywhere */
+
+// Two steps: 
+// 1. Declaration ( var, let, const) (var is outdated) 
+// 2. Assignment ( = assignment operator) 
+
+// let variable allows value to be change later 
+let firstName = "Thinh"; // string 
+let age = "26"; // string 
+let age2 = 26; // number 
+let student = true; // boolean 
+
+// const is a variable that the value cannot be change
+// const is more prefer
+const birthDate = "November 30, 1996";
+
+
+
+
