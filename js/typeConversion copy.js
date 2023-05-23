dc9782231a3ca4e39e2da5cd78b12c1fd7db6f1d
tphan1996 <107013661+thinhdphan@users.noreply.github.com>
2023-05-23 16:28:23 -0500

    type conversion

---
+++ js/typeConversion copy.js
@@ -0,0 +1,19 @@
+// Type conversion = change the datetype of a value to another
+//  Strings, Numbers, Booleans
+// typeof lets us know the datatype
+
+// User input accepts only string
+// Use number to convert string to number
+
+let age = window.prompt("How old are you?");
+
+age = Number(age);
+
+// ` ` Ternaray operator lets us use ${variableHere}
+console.log(`you are ${age} years old!`);
+
+// Converting
+// Number()
+// String()
+// Boolean() ( converting empty string = false )
+
