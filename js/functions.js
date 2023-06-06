ecb847f1a8e6a7f92dcca99225c769a4831deaaf
tphan1996 <107013661+thinhdphan@users.noreply.github.com>
2023-06-06 15:33:01 -0500

    functions notes

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+// functions = Define code once, and use it many times.
+//            To perform some code, call the function name.
+// Recommended to make function name as a verb/action 
+// 
+
+
+// parameter //
+// puts a value into a function //
+// can have more than one parameter //
+
+function calculateTax(cost, taxPercent) {
+	console.log(cost * taxPercent);
+}
+
+calculateTax(2000, 0.1); // 2000 is called the argument or value passed 
+200
