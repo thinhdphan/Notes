a45b72082ab02bce6855e1e3e8de8bc49984a615
tphan1996 <107013661+thinhdphan@users.noreply.github.com>
2023-06-06 15:37:37 -0500

    template literals

---
+++ js/templateLitrals.js
@@ -0,0 +1,9 @@
+// Template literals = delimited with ``
+//                     instead of double/single quotes
+//                     allows embedded variables and expression
+
+let userName = "ThinhP"
+let items = 3;
+let total = 75;
+
+console.log(`hello ${userName}. You have ${items} in your cart. Your total is ${total}.`);
