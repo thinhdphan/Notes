388cd61892cce0d9ad4c96afc14d938d398b9437
tphan1996 <107013661+thinhdphan@users.noreply.github.com>
2023-06-06 15:28:41 -0500

    do while loops

---
+++ js/doWhileLoop.js
@@ -0,0 +1,11 @@
+// do while loop = do something,
+//                then check the codition,
+//                repeat if condition is true
+
+let userName;
+
+do{
+  userName = window.prompt("Enter your name");
+}while (userName="")
+
+console.log("hello, ", userName);
