7503050f41cc48aca9c29254b819d5d6211d53b9
tphan1996 <107013661+thinhdphan@users.noreply.github.com>
2023-06-06 15:33:27 -0500

    return statements

---
+++ js/returnStatements.js
@@ -0,0 +1,9 @@
+// return = returns a value back to the place
+//          where you invoked a function
+// A return statement ends the function immediately after 
+
+function square(x) {
+  return x * x;
+}
+const demo = square(3);
+// demo will equal 9
