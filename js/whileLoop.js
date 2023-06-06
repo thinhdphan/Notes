fd0d3632dbf8f34cbbbf6a39285a698a213dd6a7
tphan1996 <107013661+thinhdphan@users.noreply.github.com>
2023-06-06 15:28:05 -0500

    while loops

---
+++ js/whileLoop.js
@@ -0,0 +1,14 @@
+// while loop = repeat some code
+//              while some condition is true
+//              potentially infinite
+
+let userName = "";
+
+while(userName == ""){
+  userName = window.prompt("enter your name");
+};
+
+// infinite loop
+while (1 == 1){
+  console.log{"infinite loop"};
+}
