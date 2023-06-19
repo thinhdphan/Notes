d02945c53b7a82cff9b215be10622d87fbfd1924
tphan1996 <107013661+thinhdphan@users.noreply.github.com>
2023-06-19 14:16:13 -0500

    key presses

---
+++ js/keyPresses.js
@@ -0,0 +1,42 @@
+
+
+/*
+HTML
+<div id="myDiv"></div>
+
+
+CSS
+#myDiv{
+  background-color: lightgreen;
+  width: 100px;
+  height: 100px;
+  border: 1px solid green;
+  position: relative;
+}
+*/
+const myDiv = document.getElementById("myDiv");
+window.addEventListener("keydown", move);
+let x = 0;
+let y = 0;
+
+function move(event){
+
+  switch(event.key){
+    case "ArrowDown":
+      y+=5;
+      myDiv.style.top = y + "px";
+      break;
+    case "ArrowUp":
+      y-=5;
+      myDiv.style.top = y + "px";
+      break;
+    case "ArrowRight":
+      x+=5;
+      myDiv.style.left = x + "px";
+      break;
+    case "ArrowLeft":
+      x-=5;
+      myDiv.style.left = x + "px";
+      break;
+  }
+}
